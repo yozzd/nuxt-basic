84bd9cfbdba90e3b971b294ca0374caa1c4d90c8
yozz <yozzdharma@gmail.com>
2022-09-03 11:24:59 +0700

    feat(frontend): add signup link

--- frontend/components/index/Login.vue
+++ frontend/components/index/Login.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="min-h-screen grid grid-cols-2">
     <div class="bg-gray-50"></div>
-    <div class="grid grid-cols-1 gap-8 content-center xl:px-32 2xl:px-72">
+    <div class="grid grid-cols-1 content-center xl:px-32 2xl:px-72">
       <div class="text-3xl font-bold text-center text-blue-600">
         App
       </div>
-      <IndexErrorHandler
-        v-if="errors.length"
-        :errors="errors"
-      />
-      <div class="grid grid-cols-1 gap-y-2 text-gray-400 text-center">
+      <div class="grid grid-cols-1 gap-y-2 my-8 text-gray-400 text-center">
         <div>
           Welcome to <span class="text-blue-600">App</span>
         </div>
         <div>Please login with your credentials</div>
       </div>
+      <IndexErrorHandler
+        v-if="errors.length"
+        :errors="errors"
+      />
       <div>
         <el-form
           ref="form"
@@ -41,6 +41,12 @@
             </el-button>
           </el-form-item>
         </el-form>
+      </div>
+      <div class="flex justify-center space-x-2">
+        <div class="text-gray-400">Don't have an account?</div>
+        <nuxt-link to="signup">
+          Sign Up
+        </nuxt-link>
       </div>
     </div>
   </div>
