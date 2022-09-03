42ec4d9ef5b75ca047b587616bf9987b9a01b81b
yozz <yozzdharma@gmail.com>
2022-09-03 14:02:17 +0700

    feat(backend): user model

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const router = require('./router');
+const { sequelize, config: { host, port } } = require('./config');
 
 const app = express();
-const port = 5001;
 
 app.use(cors());
 app.use(express.json());
-router(app);
-app.listen(port, () => console.log(`Server running on port ${port}`));
+
+const connect = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+const start = async () => {
+  try {
+    router(app);
+    await app.listen({ port, host }, () => console.log(`Server running on port ${port}`));
+    await connect();
+  } catch (err) {
+    process.exit(1);
+    throw new Error(err);
+  }
+};
+
+start();
