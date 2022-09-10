a556799dce42c90bcbe576f44c76e38102f33f54
yozz <yozzdharma@gmail.com>
2022-09-10 08:52:27 +0700

    feat(frontend): remove dashboard links

--- frontend/components/index/Dashboard.vue
+++ frontend/components/index/Dashboard.vue
@@ -1,14 +1,5 @@
 <template>
-  <div class="flex flex-col space-y-4">
-    <div>Examples:</div>
-    <ul class="list-disc list-inside">
-      <li>
-        <NuxtLink to="/example1">
-          Example 1: Table with pagination
-        </NuxtLink>
-      </li>
-      <li>Two</li>
-      <li>Three</li>
-    </ul>
+  <div>
+    Dashboard
   </div>
 </template>
