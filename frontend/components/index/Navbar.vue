7b165fb2e5f7d537fae946891200da3aa3ebd1ba
yozz <yozzdharma@gmail.com>
2022-09-10 10:04:25 +0700

    feat(frontend): navbar hide/show link by role

--- frontend/components/index/Navbar.vue
+++ frontend/components/index/Navbar.vue
@@ -6,6 +6,25 @@
       </NuxtLink>
     </div>
     <div class="flex-1"></div>
+    <div class="flex space-x-2">
+      <el-dropdown
+        v-if="$auth.user.role === 'admin'"
+        trigger="click"
+        @command="handleManage"
+      >
+        <el-link
+          type="primary"
+          :underline="false"
+        >
+          Manage <IconArrowDownSLine fill="#2563EB" />
+        </el-link>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item command="book">
+            Book
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
     <div>
       <el-dropdown trigger="click" @command="handleCommand">
         <el-link
@@ -27,8 +46,11 @@
 <script>
 export default {
   methods: {
-    async handleCommand(name) {
-      if (name === 'logout') await this.$auth.logout();
+    async handleCommand(command) {
+      if (command === 'logout') await this.$auth.logout();
+    },
+    handleManage(name) {
+      this.$router.push({ name });
     },
   },
 };
